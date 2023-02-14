a2abd36aa555a167a0b792872b7ee769c51c296f
Facundo <facundotisserasorribas@gmail.com>
2023-02-13 22:05:50 -0300

    servicios rch terminado

--- rch-tolvas/src/components/service/Service.jsx
+++ rch-tolvas/src/components/service/Service.jsx
@@ -7,31 +7,40 @@ import soldador from './../../assets/img/servicios/SOLDADOR.png';
 
 function Service() {
     return (
-        <div>
-            <div>
+        <div id='servicios'>
+            <div className='container-titulo'>
                 <h2>NUESTROS SERVICIOS</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, cum voluptates fugiat ab laborum, magni dolorum consectetur accusantium debitis veniam aliquid</p>
             </div>
-            <div>
-                <div>
+            <div className='servicios'>
+                <div className='caja-servicios'>
                     <h3>CAMBIO DE RODAMIENTOS</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                    <img src={rodamientos} alt="RODAMIENTOS" />
+                        <div className='texto-imagen-servicio'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+
+                            <img src={rodamientos} alt="RODAMIENTOS" className='img-servicios'  />
+                        </div>
                 </div>
-                <div>
+                <div className='caja-servicios'>
                     <h3>ACONDICIONAMIENTO DE USADOS</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                    <img src={pintor} alt="" />
+                        <div className='texto-imagen-servicio'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                            <img src={pintor} alt="" className='img-servicios' />
+                        </div>
                 </div>
-                <div>
+                <div className='caja-servicios'>
                     <h3>SOLDADURAS EN GENERAL</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                    <img src={soldador} alt="" />
+                        <div className='texto-imagen-servicio'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                            <img src={soldador} alt="" className='img-servicios' />
+                    </div>
                 </div>
-                <div>
+                <div className='caja-servicios'>
                     <h3>ENGRACE DE TOLVAS</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                    <img src={engrace} alt="" />
+                        <div className='texto-imagen-servicio'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                            <img src={engrace} alt="" className='img-servicios' />
+                        </div>
                 </div>
             </div>
         </div>
